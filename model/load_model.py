58bff60c961f0bf30429f326d72a9f2e34274d2f
Soham Sharma <42056502+soham2710@users.noreply.github.com>
2024-07-18 18:40:23 +0530

    Delete model directory

--- model/load_model.py
+++
@@ -1,6 +0,0 @@
-from transformers import T5ForConditionalGeneration, T5Tokenizer
-
-def load_model():
-    model = T5ForConditionalGeneration.from_pretrained("model/fine_tuned_t5")
-    tokenizer = T5Tokenizer.from_pretrained("model/fine_tuned_t5")
-    return model, tokenizer
