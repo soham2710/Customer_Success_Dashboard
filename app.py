cfec4a3400e488497b0072a78c15a4df37740e55
Soham Sharma <42056502+soham2710@users.noreply.github.com>
2024-07-20 04:02:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def introduction_page():
         st.markdown(
             """
             <div style="display: flex; align-items: center; height: 150px;">
-                <img src="https://github.com/soham2710/Customer_Success_Dashboard/raw/main/1646298303913.jpg" style="height: 100%; width: auto;"/>
+                <img src="https://github.com/soham2710/Customer_Success_Dashboard/raw/main/1646298303913.jpg" style="height: 100%; width: 30%;"/>
             </div>
             """,
             unsafe_allow_html=True
